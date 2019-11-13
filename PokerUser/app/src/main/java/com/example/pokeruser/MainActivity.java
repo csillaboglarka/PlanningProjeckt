fc40b088300161422e296de8f56afe6362426f8c
csillaboglarka <bendecsilla@yahoo.com>
2019-11-13 18:09:14 +0200

    Firebase connection

--- PokerUser/app/src/main/java/com/example/pokeruser/MainActivity.java
+++ PokerUser/app/src/main/java/com/example/pokeruser/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.admin;
+package com.example.pokeruser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,16 +8,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        myRef.setValue("User!");
     }
 }
